7e397b7db2924e3f64b1f2ac4bad6957813ebb57
tbreweric <trb216@ic.ac.uk>
2024-04-05 15:40:40 +0100

    Pushing run_simulation() changes to a new branch recently branched from main to avoid compatability issues. Removed add_event_listeners() from run_simulation() and added documentation for the function

---
+++ R/model.R
@@ -0,0 +1,32 @@
+#' Run the simulation
+#'
+#' @param parameters A list of the model parameters as generated using `get_parameters`
+#' @param timesteps An integer representing the number of timesteps to simulate
+#'
+#' @export
+run_simulation <- function(parameters, timesteps) {
+
+  # Generate the model variables:
+  variables <- create_variables(parameters)
+
+  # Generate the model events:
+  events <- create_events(parameters)
+
+  # Initialise the model events: (Not currently needed, but may be when we add interventions)
+  #initialise_events(events, variables, parameters)
+
+  # Set up the model renderer:
+  renderer <- individual::Render$new(timesteps)
+
+  # Generate the model processes:
+  processes <- create_processes(variables, events, parameters)
+
+  # Use individual::simulation_loop() to run the model for the specified number of timesteps
+  individual::simulation_loop(
+    processes = processes,
+    variables = variables,
+    events = unlist(events),
+    timesteps = timesteps
+  )
+  renderer$to_dataframe()
+}
