b3527c6e51688dc12568d3e894e1189a32f430e3
tbreweric <trb216@ic.ac.uk>
2024-04-05 15:24:03 +0100

    Removed the add_event_listeners() function call in run_simulation(). I have left initialise_events() as this may be required when we add in interventions

--- R/model.R
+++ R/model.R
@@ -1,7 +1,7 @@
 #' Run the simulation
 #'
 #' @param parameters A list of the model parameters as generated using `get_parameters`
-#' @param timesteps An integer of the number of timesteps, in days, to simulate
+#' @param timesteps An integer representing the number of timesteps to simulate
 #'
 #' @export
 run_simulation <- function(parameters, timesteps) {
@@ -12,31 +12,18 @@ run_simulation <- function(parameters, timesteps) {
   # Generate the model events:
   events <- create_events(parameters)
 
-  # Initialise the model events: (NOT SURE IF NEEDED YET)
+  # Initialise the model events: (Not currently needed, but may be when we add interventions)
   #initialise_events(events, variables, parameters)
 
   # Set up the model renderer:
   renderer <- individual::Render$new(timesteps)
-
-  # Attached the listeners to the model events:
-  attach_event_listeners(
-    events,
-    variables,
-    parameters,
-    correlations,
-    renderer
-  )
 
   # Generate the model processes:
   processes <- create_processes(variables, events, parameters)
 
   # Use individual::simulation_loop() to run the model for the specified number of timesteps
   individual::simulation_loop(
-    processes = create_processes(
-      renderer,
-      variables,
-      events,
-      parameters),
+    processes = processes,
     variables = variables,
     events = unlist(events),
     timesteps = timesteps
