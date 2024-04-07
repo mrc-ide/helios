90f161ffe2f494ab9e8ca5652c70307636e7d977
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-04-07 20:39:50 +0100

    Weird scoping argument mix-up issue with running the model fixed. All inputs/arguments are called *_list e.g. parameters_list, variables_list etc from now on and are passed by reference through functions. Run simulation function now works.

--- R/model.R
+++ R/model.R
@@ -1,32 +1,34 @@
 #' Run the simulation
 #'
-#' @param parameters A list of the model parameters as generated using `get_parameters`
+#' @param parameters_list A list of the model parameters as generated using `get_parameters`
 #' @param timesteps An integer representing the number of timesteps to simulate
 #'
 #' @export
-run_simulation <- function(parameters, timesteps) {
+run_simulation <- function(parameters_list, timesteps) {
 
   # Generate the model variables:
-  variables <- create_variables(parameters)
+  variables_list <- create_variables(parameters_list)
 
   # Generate the model events:
-  events <- create_events(parameters, variables)
+  events_list <- create_events(variables_list = variables_list, parameters_list = parameters_list)
 
   # Initialise the model events: (Not currently needed, but may be when we add interventions)
   #initialise_events(events, variables, parameters)
 
   # Set up the model renderer:
-  timesteps <- round(parameters$simulation_time/parameters$dt)
+  timesteps <- round(parameters_list$simulation_time/parameters_list$dt)
   renderer <- individual::Render$new(timesteps)
 
   # Generate the model processes:
-  processes <- create_processes(variables, events, parameters)
+  processes_list <- create_processes(variables_list = variables_list,
+                                     events_list = events_list,
+                                     parameters_list = parameters_list)
 
   # Use individual::simulation_loop() to run the model for the specified number of timesteps
   individual::simulation_loop(
-    variables = variables,
-    events = unlist(events),
-    processes = processes,
+    variables = variables_list,
+    events = unlist(events_list),
+    processes = processes_list,
     timesteps = timesteps
   )
   renderer$to_dataframe()
--- inst/testing_process_and_modelSim.R
+++ inst/testing_process_and_modelSim.R
@@ -3,53 +3,37 @@ library(individual)
 
 ## Create parameters
 source("R/parameters.R")
-params <- get_parameters()
-params$seed <- 10
+parameters_list <- get_parameters()
+parameters_list$seed <- 10
+parameters_list$beta_household <- 1.0
 
 ## Generate the model variables:
 source("R/variables.R")
 source("R/sampling.R")
-variables <- create_variables(params)
+variables_list <- create_variables(parameters_list)
 
 ## Generate the model events:
 source("R/events.R")
-events <- create_events(params, variables)
+events_list <- create_events(variables_list = variables_list, parameters_list = parameters_list)
 
 # Set up the model renderer:
-timesteps <- round(params$simulation_time/params$dt)
+timesteps <- round(parameters_list$simulation_time/parameters_list$dt)
 renderer <- individual::Render$new(timesteps)
 
 # Generate the model processes:
 source("R/processes.R")
-processes <- create_processes(variables, events, params)
+processes_list <- create_processes(variables_list = variables_list,
+                                   events_list = events_list,
+                                   parameters_list = parameters_list)
 
 # Use individual::simulation_loop() to run the model for the specified number of timesteps
 individual::simulation_loop(
-  variables = variables,
-  events = unlist(events),
-  processes = create_processes(variables, events, params),
+  variables = variables_list,
+  events = events_list,
+  processes = processes_list,
   timesteps = timesteps
 )
 
 source("R/model.R")
-run_simulation(params, 10)
-#
-# x <- SE_process(parameters = params, variables = variables)
-# x(7)
-#
-# for (t in seq_len(timesteps)) {
-#   for (process in processes) {
-#     individual:::execute_any_process(processes[[1]], 1)
-#   }
-#   for (event in events) {
-#     event$.process()
-#   }
-#   for (variable in variables) {
-#     variable$.update()
-#   }
-#   for (event in events) {
-#     event$.tick()
-#   }
-# }
-#
-#
+run_simulation(parameters_list, 10)
+
