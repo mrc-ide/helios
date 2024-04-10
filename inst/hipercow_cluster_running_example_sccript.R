a30d33239ffa926ad9bf6f037bc9697876cc5041
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-04-10 21:59:51 +0100

    in progress updates to get parallelism working

--- inst/hipercow_cluster_running_example_sccript.R
+++ inst/hipercow_cluster_running_example_sccript.R
@@ -32,44 +32,68 @@ hipercow_provision_list()
 hipercow_provision_check()
 hipercow_provision_compare()
 hipercow_resources()
+hipercow_configuration()
+hipercow_provision_check()
 
 ## Testing helios running locally
 parameters_list <- helios::get_parameters()
 test_run_local <- helios::run_simulation(parameters_list = parameters_list, timesteps = 50)
 
 ## Testing helios running on cluster
+hipercow_environment_create(packages = c("helios", "individual"))
 id <- task_create_expr(helios::run_simulation(parameters_list = parameters_list, timesteps = 50))
-task_info(id)$times
-task_info(id)$data
-task_log_show(id)
-task_log_show(id, outer = TRUE)
-task_log_watch(id)
 task_status(id)
 task_result(id)
+task_info(id)
+task_log_watch(id)
 
 ## Getting parallel version set up
 resources <- hipercow_resources(cores = 5)
-id2 <- task_create_expr(
-  parallel::clusterApply(NULL, 1:10, function(x) helios::run_simulation(parameters_list = parameters_list, timesteps = 50)),
+hipercow_environment_create(packages = c("helios", "individual"))
+
+id2 <- task_create_call(
+  fn = run_simulation,
+  args = list(parameters_list_of_lists),
   parallel = hipercow_parallel("parallel"),
   resources = resources)
 task_status(id2)
+task_log_watch(id2)
 task_result(id2)
 
-resources <- hipercow_resources(cores = 5)
-parameters_list_of_lists <- vector(mode = "list", length = 10)
-for (i in 1:length(parameters_list_of_lists)) {
-  parameters_list_of_lists[[i]] <- parameters_list
-  parameters_list_of_lists[[i]]$population_size <- rpois(n = 1, lambda = 10000)
+## Create list of parameter lists
+params <- helios::get_parameters()
+list_params <- vector(mode = "list", length = 10)
+for (i in 1:length(list_params)) {
+  list_params[[i]] <- params
 }
+
+## Using task_create_exp and clusterApply
+id <- task_create_expr(
+  expr = parallel::clusterApply(cl = NULL, x = 1:10, fun = function(i) {
+    helios::run_simulation(parameters_list = list_params[[i]], timesteps = 50)
+  }),
+  environment = "default",
+  parallel = hipercow_parallel("parallel"),
+  resources = hipercow_resources(cores = 5))
+task_log_watch(id)
+
+## Using task_create_explicit instead to try and export list_params
+id2 <- task_create_explicit(
+  expr = parallel::clusterApply(cl = NULL, x = 1:10, fun = function(i) {
+    helios::run_simulation(parameters_list = list_params[[i]], timesteps = 50)
+  }),
+  export = "list_params",
+  envir = parent.frame(),
+  parallel = hipercow_parallel("parallel"),
+  resources = hipercow_resources(cores = 5))
+task_log_watch(id2)
+
+## Create and submit task using parLapply
 id3 <- task_create_expr(
-  parallel::parLapply(cl = NULL, X = parameters_list_of_lists, fun = function(i) {
+  expr = parallel::parLapply(cl = NULL, X = list_params, fun = function(i) {
     helios::run_simulation(parameters_list = i, timesteps = 50)
   }),
-  parallel = hipercow_parallel("parallel"),
-  resources = resources)
-task_status(id3)
-task_result(id3)
-
-
-
+  environment = "default",
+  parallel = hipercow_parallel(method = "parallel", environment = "default"),
+  resources =  hipercow_resources(cores = 5))
+task_log_watch(id3)
