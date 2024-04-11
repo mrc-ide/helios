2e50cb6b56d6fe382575f624405a63a6f2a23378
athowes <adamthowes@gmail.com>
2024-04-11 15:00:02 +0100

    Add empirical option for schools

--- R/parameters.R
+++ R/parameters.R
@@ -65,6 +65,7 @@ get_parameters <- function(overrides = list()) {
     dt = 0.5, # check this as default
     simulation_time = 150,
     household_distribution_generation = "empirical",
+    school_distribution_generation = "empirical",
     endemic_or_epidemic = "epidemic",
     duration_immune = NULL
   )
