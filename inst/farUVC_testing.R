f057a71b08caf7a43210cefe9a9c694acb29bffb
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-04-14 06:48:21 +0100

    playing around with scrap script

--- inst/farUVC_testing.R
+++ inst/farUVC_testing.R
@@ -11,12 +11,12 @@ source("R/model.R")
 source("R/utils.R")
 
 ## Running the model with no farUVC
-nofarUVC_parameters_list <- get_parameters(overrides = list(beta_household = 0.15,
-                                                            beta_workplace = 0.15,
-                                                            beta_school = 0.15,
-                                                            beta_leisure = 0.15,
+nofarUVC_parameters_list <- get_parameters(overrides = list(beta_household = 0.1,
+                                                            beta_workplace = 0.1,
+                                                            beta_school = 0.1,
+                                                            beta_leisure = 0.1,
                                                             beta_community = 0.05,
-                                                            simulation_time = 75,
+                                                            simulation_time = 300,
                                                             number_initially_exposed = 50))
 nofarUVC_output <- run_simulation(nofarUVC_parameters_list)
 health_cols <-  c("royalblue3","firebrick3","darkorchid3", "orange2")
