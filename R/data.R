54511f7eefe075f9abbdd4989b48e80d29cc1728
athowes <adamthowes@gmail.com>
2024-04-15 11:50:03 +0100

    Add data documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Data on English schools
+#'
+#' A data frame with 24,442 rows and 268 columns.
+#'
+#' @source <https://explore-education-statistics.service.gov.uk/find-statistics/school-pupils-and-their-characteristics/2022-23>
+"schools_england"
+
+#' Data on on baseline household demographics from Hinch et al.
+#'
+#' A data frame with 10,000 rows and 9 columns.
+#'
+#' @source <UNKOWN>
+"baseline_household_demographics"
