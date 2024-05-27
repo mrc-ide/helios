6638331be076c478b1d2be0575035356b43d8a09
tbreweric <trb216@ic.ac.uk>
2024-05-27 12:12:26 +0100

    Added unit tests to support changes to get_parameters()

--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -8,3 +8,24 @@ test_that("get_parameters() errors when a setting-specific beta has length great
                regexp = "ERROR: A setting-specific beta has length not equal to 1. All setting-specific betas must be of length 1")
 
 })
+
+test_that("get_parameters() errors when the endemic setting is switched on without parameterising duration_immune", {
+
+  expect_error(object = get_parameters(overrides = list(
+    endemic_or_epidemic = 'endemic',
+    prob_inf_external = 0.01)
+  ),
+  regexp = "duration_immune must be specified if endemic_or_epidemic is set to endemic")
+
+})
+
+test_that("get_parameters() errors when the endemic setting is switched on without parameterising duration_immune", {
+
+  expect_error(object = get_parameters(overrides = list(
+    endemic_or_epidemic = 'endemic',
+    duration_immune = 14)
+  ),
+  regexp = "prob_inf_external must be specified if endemic_or_epidemic is set to endemic")
+
+})
+
