6b7c10b3307ea477d811b5a160acb1c2e9137483
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-07-11 17:45:32 +0100

    change to dataset_raw to incorpoate _uk addition to name

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,8 @@
 # Make any changes to the saved data for the package here
 library(readr)
 
-baseline_household_demographics <- read_csv("data-raw/Hinch_et_al_baseline_household_demographics.csv")
-usethis::use_data(baseline_household_demographics)
+baseline_household_demographics_uk <- read_csv("data-raw/Hinch_et_al_baseline_household_demographics.csv")
+usethis::use_data(baseline_household_demographics_uk)
 
 schools_england <- read_csv("data-raw/spc_school_level_underlying_data_23112023.csv")
 usethis::use_data(schools_england)
