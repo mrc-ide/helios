e02cca395e234aeff99dacc22c02aef2e1ed4bb9
tbreweric <trb216@ic.ac.uk>
2024-07-22 00:03:09 +0100

    Added a calculate_setting_specific_riskiness() function to setting_specific_risk.R to calculate the vector of riskinesses for a defined setting. This function will be called in create_processes for each setting and replace the loops currently in place

--- R/setting_specific_risk.R
+++ R/setting_specific_risk.R
@@ -35,6 +35,42 @@ set_setting_specific_risk <- function(parameter_list, setting, mean, sd, min, ma
 
 }
 
+#' calculate_setting_specific_riskiness
+#'
+#' @param parameters_list A list of parameters as generated using `get_parameters`
+#' @param setting A character string describing the setting in which Far UVC is being deployed
+#' @param number_of_locations The number of locations for the setting-type for which riskiness values are to be drawn
+#'
+#' @family setting-specific riskiness
+#' @export
+calculate_setting_specific_riskiness <- function(parameters_list, setting, number_of_locations) {
 
+  # Ensure only a single setting passed to the function:
+  if(length(setting) > 1) {
+    stop("Error: Number of settings input greater than 1, parameterise for one setting at a time")
+  }
+
+  # Check that the setting supplied is from the accepted list:
+  if(setting != "workplace" & setting != "school" & setting != "leisure" & setting != "household") {
+    stop("Error: Input setting invalid - far UVC only deployable in workplace, school, leisure, or household settings")
+  }
+
+  # If setting-specific riskiness is switched on for the specified setting type, calculate setting-specific
+  # riskiness values for each location within the setting using the rlnormTrunc() function:
+  if(parameters_list[[paste0("setting_specific_riskiness_", setting)]]) {
+    setting_specific_riskiness <- EnvStats::rlnormTrunc(n = number_of_locations,
+                                                        meanlog = parameters_list[[paste0("setting_specific_riskiness_", setting, "_meanlog")]],
+                                                        sdlog = parameters_list[[paste0("setting_specific_riskiness_", setting, "sdlog")]],
+                                                        min = parameters_list[[paste0("setting_specific_riskiness_", setting, "_min")]],
+                                                        max = parameters_list[[paste0("setting_specific_riskiness_", setting, "_max")]])
+  } else {
+
+    # If setting-specific riskiness is switched off for the specified setting type, assign each location a 1:
+    setting_specific_riskiness <- rep(1, number_of_locations)
+  }
+
+  # Return the vector of setting-specific riskiness values:
+  return(setting_specific_riskiness)
+}
 
 
