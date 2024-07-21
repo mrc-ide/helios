44afefb8255684e5cd4af7d32b99a2168930bb05
tbreweric <trb216@ic.ac.uk>
2024-07-22 00:27:13 +0100

    Placed generate_setting_specific_riskiness() calls in create_processes()

--- R/setting_specific_risk.R
+++ R/setting_specific_risk.R
@@ -1,6 +1,6 @@
 #' set_setting_specific_riskiness
 #'
-#' @param parameter_list A list of parameters as generated using `get_parameters`
+#' @param parameters_list A list of parameters as generated using `get_parameters`
 #' @param setting A character string describing the setting in which Far UVC is being deployed
 #' @param mean The mean for the truncated log-normal distribution from which riskiness values will be drawn
 #' @param sd The standard deviation for the truncated log-normal distribution from which riskiness values will be drawn
@@ -9,7 +9,7 @@
 #'
 #' @family setting-specific riskiness
 #' @export
-set_setting_specific_risk <- function(parameter_list, setting, mean, sd, min, max) {
+set_setting_specific_risk <- function(parameters_list, setting, mean, sd, min, max) {
 
   # Ensure only a single setting passed to the function:
   if(length(setting) > 1) {
@@ -25,13 +25,13 @@ set_setting_specific_risk <- function(parameter_list, setting, mean, sd, min, ma
   parameter_list[[paste0(setting, "_specific_riskiness_workplace")]] <- TRUE
 
   # Overwrite the log-normal distribution parameters for the specified setting:
-  parameter_list[[paste0(setting, "_specific_riskiness_workplace_meanlog")]] <- mean
-  parameter_list[[paste0(setting, "_specific_riskiness_workplace_sdlog")]] <- sd
-  parameter_list[[paste0(setting, "_specific_riskiness_workplace_min")]] <- min
-  parameter_list[[paste0(setting, "_specific_riskiness_workplace_max")]] <- max
+  parameters_list[[paste0(setting, "_specific_riskiness_workplace_meanlog")]] <- mean
+  parameters_list[[paste0(setting, "_specific_riskiness_workplace_sdlog")]] <- sd
+  parameters_list[[paste0(setting, "_specific_riskiness_workplace_min")]] <- min
+  parameters_list[[paste0(setting, "_specific_riskiness_workplace_max")]] <- max
 
   # Return the modified paramter list:
-  return(parameter_list)
+  return(parameters_list)
 
 }
 
