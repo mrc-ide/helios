bf420a35fac554f8f9ccb8ca82c3eacad61aabbf
athowes <adamthowes@gmail.com>
2024-08-13 17:20:33 +0100

    Outline of work to be done for joint far UVC

---
+++ inst/square-feet.R
@@ -0,0 +1,11 @@
+# Loading library
+library(helios)
+
+# Pass in square feet per person for each setting type
+# Seperate or joint as another parameter
+# New "joint_allocation" function
+# Master parameters and then the setting specific parameters inherit if not specified
+# For this function don't need targetted
+# For this function also don't need buildings
+# Just individual and just random
+# Get rid of buildings version from other part of tree
