9a4a5e82844d9340a1c18b550cdd1b835d106834
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-09-03 11:10:10 +0100

    provisional endemic figures

--- inst/blueprint_output_3_Sep9/Report_3_Endemic/draft_endemic_figure_production_CW.R
+++ inst/blueprint_output_3_Sep9/Report_3_Endemic/draft_endemic_figure_production_CW.R
@@ -55,24 +55,9 @@ avg_summary_outputs <- summary_outputs %>%
   summarise(mean_reduction_incidence = mean(reduction_incidence),
             mean_reduction_prevalence = mean(reduction_prevalence),
             lower_reduction_incidence = min(reduction_incidence),
-            upper_reduction_incidence = max(reduction_incidence))
-
-# ggplot(subset(avg_summary_outputs, coverage_type == "random" & coverage <= 0.7),
-#               aes(x = 100 * coverage, y = 100 * mean_reduction_incidence)) +
-#   geom_bar(stat = "identity", position = "dodge", fill = "#324A5F", col = "black") +
-#   geom_errorbar(aes(ymin = 100 * lower_reduction_incidence,
-#                     ymax = 100 * upper_reduction_incidence),
-#                 width = 5) +
-#   theme_bw() +
-#   facet_grid(archetype ~ efficacy, scales = "free_y",
-#              labeller = as_labeller(c(`0.4` = "40% Efficacy",
-#                                       `0.6` = "60% Efficacy",
-#                                       `0.8` = "80% Efficacy",
-#                                       `flu` = "Influenza",
-#                                       `sars_cov_2` = "SARS-CoV-2"))) +
-#   scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 80, 100)) +
-#   labs(fill = "Time\nPeriod", x = "Far UVC Coverage (%)",
-#        y = "% Reduction in Annual Infection Incidence")
+            upper_reduction_incidence = max(reduction_incidence)) %>%
+  ungroup() %>%
+  mutate(new_ID = 1:n())
 
 ggplot(data = subset(avg_summary_outputs, coverage_type == "random" & coverage <= 0.7),
        aes(x = 100 * coverage, y = 100 * mean_reduction_incidence)) +
@@ -94,6 +79,43 @@ ggplot(data = subset(avg_summary_outputs, coverage_type == "random" & coverage <
   labs(fill = "Time\nPeriod", x = "Far UVC Coverage (%)",
        y = "% Reduction in Annual Infection Incidence")
 
+avg_summary_outputs_diff <- avg_summary_outputs %>%
+  filter(!is.na(coverage_type),
+         !is.na(coverage),
+         !is.na(efficacy)) %>%
+  pivot_wider(id_cols = c(archetype, coverage, efficacy),
+              names_from = coverage_type,
+              values_from = mean_reduction_incidence) %>%
+  mutate(diff_abs = targeted_riskiness - random,
+         diff_rel = targeted_riskiness / random - 1)
+
+ggplot(data = subset(avg_summary_outputs_diff, coverage <= 0.9),
+       aes(x = 100 * coverage, y = 100 * diff_abs)) +
+  geom_bar(stat = "identity", position = "dodge", fill = "#324A5F", col = "black") +
+  theme_bw() +
+  facet_grid(archetype ~ efficacy, scales = "free_y",
+             labeller = as_labeller(c(`0.4` = "40% Efficacy",
+                                      `0.6` = "60% Efficacy",
+                                      `0.8` = "80% Efficacy",
+                                      `flu` = "Influenza",
+                                      `sars_cov_2` = "SARS-CoV-2"))) +
+  scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(fill = "Time\nPeriod", x = "Far UVC Coverage (%)",
+       y = "Absolute Difference in % Reduction in Annual Infection Incidence")
+
+ggplot(data = subset(avg_summary_outputs_diff, coverage <= 0.9),
+       aes(x = 100 * coverage, y = 100 * diff_rel)) +
+  geom_bar(stat = "identity", position = "dodge", fill = "#324A5F", col = "black") +
+  theme_bw() +
+  facet_grid(archetype ~ efficacy, scales = "free_y",
+             labeller = as_labeller(c(`0.4` = "40% Efficacy",
+                                      `0.6` = "60% Efficacy",
+                                      `0.8` = "80% Efficacy",
+                                      `flu` = "Influenza",
+                                      `sars_cov_2` = "SARS-CoV-2"))) +
+  scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
+  labs(fill = "Time\nPeriod", x = "Far UVC Coverage (%)",
+       y = "Extra % |Impact")
 
 # Loading and processing the full outputs
 combined_outputs <- data.frame()
@@ -127,71 +149,4 @@ ggplot(combined_outputs_summarised, aes(x = timestep,
                              group = interaction(coverage_type, factor(iteration)))) +
   geom_line() +
   facet_grid(archetype ~ efficacy)
-
-
-
-
-
-
-# Calculate the timestep on which to switch Far UVC on:
-timestep_uvc_on <- round(((20 - 2) * 365) / 0.5)
-
-# Create a long-format version of the combined dataframe:
-combined_outputs |>
-  pivot_longer(cols = c(S_count, E_count, I_count, R_count),
-               names_to = "state",
-               values_to = "individuals") -> combined_outputs_long
-
-#----- 2) Mean number of infected individuals through time (by iteration) --------------------------
-
-# SARS-CoV-2
-combined_outputs_long |>
-  filter(timestep >= ((365 * 15 * 2) + 1)) |>
-  filter(archetype == "sars_cov_2") |>
-  filter(state == "I_count") |>
-  summarise(I = mean(individuals), .by = c(archetype, coverage_type, coverage, efficacy, timestep)) |>
-  ggplot(aes(x = timestep, y = I, colour = factor(efficacy))) +
-  geom_line() +
-  theme_bw() +
-  labs(col = "Efficacy",
-       x = "Time (Days)",
-       y = "Mean Number of Infectious Individuals") +
-  geom_vline(xintercept = timestep_uvc_on, linetype = "dashed") +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  facet_grid(coverage_type~coverage)
-
-# Influenza
-combined_outputs_long |>
-  filter(timestep >= ((365 * 15 * 2) + 1)) |>
-  filter(archetype == "flu") |>
-  filter(state == "I_count") |>
-  summarise(I = mean(individuals), .by = c(archetype, coverage_type, coverage, efficacy, timestep)) |>
-  ggplot(aes(x = timestep, y = I, colour = factor(efficacy))) +
-  geom_line() +
-  theme_bw() +
-  labs(col = "Efficacy",
-       x = "Time (Days)",
-       y = "Mean Number of Infectious Individuals") +
-  geom_vline(xintercept = timestep_uvc_on, linetype = "dashed") +
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0)) +
-  facet_grid(coverage_type~coverage)
-
-
-#----- 3) Charlie's Report 2 Figure 1: Infectious Dynamics -----------------------------------------
-
-#----- 3) Charlie's Report 2 Figure 1: Infectious Dynamics -----------------------------------------
-
-# Load an example parameter list:
-example_parameter_list <- readRDS("./Report_3_Endemic/endemic_parameter_list_1.rds")
-
-# Get the time and human population parameters for plotting:
-dt <- example_parameter_list[[1]]$dt
-population <- example_parameter_list[[1]]$human_population
-years_to_simulate <- example_parameter_list[[1]]$simulation_time / 365
-timestep_baseline_start <- ((years_to_simulate - 5) * 365) / dt
-timestep_baseline_end <- ((years_to_simulate - 2) * 365) / dt
-timestep_uvc_start <- ((years_to_simulate - 2) * 365 + 1) / dt
-timestep_uvc_end <- (years_to_simulate * 365) / dt
 
