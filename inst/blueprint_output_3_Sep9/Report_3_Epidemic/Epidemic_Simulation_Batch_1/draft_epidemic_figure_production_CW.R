93588ebc6ab0b621e9ae62472f503c1c93567446
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-09-05 11:29:48 +0100

    beginnings of epidemic plots

--- inst/blueprint_output_3_Sep9/Report_3_Epidemic/Epidemic_Simulation_Batch_1/draft_epidemic_figure_production_CW.R
+++ inst/blueprint_output_3_Sep9/Report_3_Epidemic/Epidemic_Simulation_Batch_1/draft_epidemic_figure_production_CW.R
@@ -17,6 +17,7 @@ output_files <- output_files[which(stringr::str_detect(string = output_files, pa
 
 # Quick visualisation of key metrics for endemic#
 summary_outputs <- data.frame()
+counter <- 1
 for(i in 1: length(output_files)) {
 
   # Load in the output file:
@@ -31,118 +32,50 @@ for(i in 1: length(output_files)) {
     } else {
       tempdf[[j]]$archetype <- "sars_cov_2"
     }
+    tempdf[[j]]$iteration <- counter
+    counter <- counter + 1
   }
 
   # Create a combined dataframe of the outputs
-  tempdf <- bind_rows(tempdf) %>%
-    group_by(archetype, coverage_type, coverage, efficacy, disease_status) %>% # missing ID, iteration and still issues with coverage_type, coverage and efficacy
-    summarise(peak = max(x$I_count),
-              peak_timing = which(x$I_count == max(x$I_count)),
-              final_size = max(x$R_count))
+  tempdf2 <- bind_rows(tempdf) %>%
+    mutate(coverage_type = ifelse(is.na(coverage_type), "none", coverage_type)) %>%
+    mutate(coverage  = ifelse(is.na(coverage), 0, coverage)) %>%
+    mutate(efficacy  = ifelse(is.na(efficacy), 0, efficacy)) %>%
+    group_by(iteration, archetype, coverage_type, coverage, efficacy, disease_status) %>%
+    summarise(peak = max(I_count),
+              peak_timing = first(which(I_count == max(I_count))),
+              final_size = max(R_count))
 
   # Bind it to the combined dataframe:
-  summary_outputs <- bind_rows(summary_outputs, tempdf)
+  summary_outputs <- bind_rows(summary_outputs, tempdf2)
 }
-
-head(summary_outputs)
 
 avg_summary_outputs <- summary_outputs %>%
   group_by(archetype, coverage_type, coverage, efficacy, disease_status) %>%
-  summarise(mean_reduction_incidence = mean(reduction_incidence),
-            mean_reduction_prevalence = mean(reduction_prevalence),
-            lower_reduction_incidence = min(reduction_incidence),
-            upper_reduction_incidence = max(reduction_incidence)) %>%
-  ungroup() %>%
-  mutate(new_ID = 1:n())
+  summarise(mean_peak_timing = mean(peak_timing),
+            mean_peak = mean(peak),
+            mean_final_size = mean(final_size))
 
-ggplot(data = subset(avg_summary_outputs, coverage_type == "random" & coverage <= 0.7),
-       aes(x = 100 * coverage, y = 100 * mean_reduction_incidence)) +
-  geom_bar(data = subset(avg_summary_outputs, coverage_type == "targeted_riskiness" & coverage <= 0.7),
-           aes(x = 100 * coverage, y = 100 * mean_reduction_incidence),
-           col = "black", fill = "lightgrey", stat = "identity") +
-  geom_bar(stat = "identity", position = "dodge", fill = "#324A5F", col = "black") +
-  geom_errorbar(aes(ymin = 100 * lower_reduction_incidence,
-                    ymax = 100 * upper_reduction_incidence),
-                width = 5) +
-  theme_bw() +
-  facet_grid(archetype ~ efficacy, scales = "free_y",
-             labeller = as_labeller(c(`0.4` = "40% Efficacy",
-                                      `0.6` = "60% Efficacy",
-                                      `0.8` = "80% Efficacy",
-                                      `flu` = "Influenza",
-                                      `sars_cov_2` = "SARS-CoV-2"))) +
-  scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 80, 100)) +
-  labs(fill = "Time\nPeriod", x = "Far UVC Coverage (%)",
-       y = "% Reduction in Annual Infection Incidence")
+ggplot() +
+  geom_line(data = subset(avg_summary_outputs, coverage_type == "random" & coverage <= 0.7),
+            aes(x = 100 * coverage, y = 100 * mean_final_size), col = "black") +
+  geom_line(data = subset(avg_summary_outputs, coverage_type == "targeted_riskiness" & coverage <= 0.7),
+            aes(x = 100 * coverage, y = 100 * mean_final_size), col = "grey") +
+  facet_grid(archetype ~ efficacy, scales = "free_y") +
+  lims(y = c(0, NA))
 
-avg_summary_outputs_diff <- avg_summary_outputs %>%
-  filter(!is.na(coverage_type),
-         !is.na(coverage),
-         !is.na(efficacy)) %>%
-  pivot_wider(id_cols = c(archetype, coverage, efficacy),
-              names_from = coverage_type,
-              values_from = mean_reduction_incidence) %>%
-  mutate(diff_abs = targeted_riskiness - random,
-         diff_rel = targeted_riskiness / random - 1)
+ggplot() +
+  geom_line(data = subset(avg_summary_outputs, coverage_type == "random" & coverage <= 0.7),
+            aes(x = 100 * coverage, y = 100 * mean_peak), col = "black") +
+  geom_line(data = subset(avg_summary_outputs, coverage_type == "targeted_riskiness" & coverage <= 0.7),
+            aes(x = 100 * coverage, y = 100 * mean_peak), col = "grey") +
+  facet_grid(archetype ~ efficacy, scales = "free_y") +
+  lims(y = c(0, NA))
 
-ggplot(data = subset(avg_summary_outputs_diff, coverage <= 0.9),
-       aes(x = 100 * coverage, y = 100 * diff_abs)) +
-  geom_bar(stat = "identity", position = "dodge", fill = "#324A5F", col = "black") +
-  theme_bw() +
-  facet_grid(archetype ~ efficacy, scales = "free_y",
-             labeller = as_labeller(c(`0.4` = "40% Efficacy",
-                                      `0.6` = "60% Efficacy",
-                                      `0.8` = "80% Efficacy",
-                                      `flu` = "Influenza",
-                                      `sars_cov_2` = "SARS-CoV-2"))) +
-  scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
-  labs(fill = "Time\nPeriod", x = "Far UVC Coverage (%)",
-       y = "Absolute Difference in % Reduction in Annual Infection Incidence")
-
-ggplot(data = subset(avg_summary_outputs_diff, coverage <= 0.9),
-       aes(x = 100 * coverage, y = 100 * diff_rel)) +
-  geom_bar(stat = "identity", position = "dodge", fill = "#324A5F", col = "black") +
-  theme_bw() +
-  facet_grid(archetype ~ efficacy, scales = "free_y",
-             labeller = as_labeller(c(`0.4` = "40% Efficacy",
-                                      `0.6` = "60% Efficacy",
-                                      `0.8` = "80% Efficacy",
-                                      `flu` = "Influenza",
-                                      `sars_cov_2` = "SARS-CoV-2"))) +
-  scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) +
-  labs(fill = "Time\nPeriod", x = "Far UVC Coverage (%)",
-       y = "Extra % |Impact")
-
-# Loading and processing the full outputs
-combined_outputs <- data.frame()
-for(i in 1: length(output_files)) {
-
-  # Load in the output file:
-  temp_obj <- readRDS(file = paste0("./Report_3_Endemic/Endemic_Simulation_Batch_2/endemic_batch_2_b_outputs/", output_files[i]))
-
-  # Filter out the parameter lists:
-  tempdf <- list()
-  for(j in 1:length(temp_obj)) {
-    tempdf[[j]] <- temp_obj[[j]][[2]]
-  }
-
-  # Create a combined dataframe of the outputs
-  tempdf <- bind_rows(tempdf)
-
-  # Bind it to the combined dataframe:
-  combined_outputs <- bind_rows(combined_outputs, tempdf) %>%
-    select(timestep, E_new, E_count, I_count, iteration, archetype, coverage_type, coverage, efficacy)
-}
-
-combined_outputs_summarised <- combined_outputs %>%
-  group_by(archetype, coverage_type, coverage, efficacy, iteration) %>%
-  filter(timestep > min(indices_prev_UVC)) %>%
-  filter(coverage == 0.5)
-
-ggplot(combined_outputs_summarised, aes(x = timestep,
-                             y = I_count,
-                             colour = coverage_type,
-                             group = interaction(coverage_type, factor(iteration)))) +
-  geom_line() +
-  facet_grid(archetype ~ efficacy)
-
+ggplot() +
+  geom_line(data = subset(avg_summary_outputs, coverage_type == "random" & coverage <= 0.7),
+            aes(x = 100 * coverage, y = 100 * mean_peak_timing), col = "black") +
+  geom_line(data = subset(avg_summary_outputs, coverage_type == "targeted_riskiness" & coverage <= 0.7),
+            aes(x = 100 * coverage, y = 100 * mean_peak_timing), col = "grey") +
+  facet_grid(archetype ~ efficacy, scales = "free_y") +
+  lims(y = c(0, NA))
